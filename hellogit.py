2eda77f4c39b11aa4500f30d7ec5a02f801844bc
danicor2688 <danicor2688@gmail.com>
2023-11-10 11:07:49 -0500

    agregado

--- hellogit.py
+++ hellogit.py
@@ -1,3 +1,5 @@
 print("ESTOY LLEVANDO CURSOS DE UDEMY PARA APRENDER BIEN GIT OK")
 
 print("solucionar problema de errores con la combinacion de ramas")
+
+print("ESTA LINEA ES NUEVA PARA LOS CAMBIOS EN MI ARCHIVO")
