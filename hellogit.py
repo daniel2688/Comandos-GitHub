5a8574072a955e5cfc2e5df066f2caa976cbb723
danicor2688 <117961516+daniel2688@users.noreply.github.com>
2023-11-12 19:59:00 -0500

    all

--- hellogit.py
+++
@@ -1,9 +0,0 @@
-print("ESTOY CAMBIANDO EL ARCHIVO EN SU TOTALIDAD !")
-print("ESTOY LLEVANDO CURSOS DE UDEMY PARA APRENDER BIEN GIT OK")
-print("ESTOY LLEVANDO CURSOS DE UDEMY PARA APRENDER GIT!")
-
-print("agregare esta nueva linea")
-
-print("solucionar problema de errores con la combinacion de ramas")
-
-print("ESTA LINEA ES NUEVA PARA LOS CAMBIOS EN MI ARCHIVO")
